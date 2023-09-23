1892fe002709f4369541854976ecbe6458116414
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-09-23 11:43:04 +0200

    links in ul, not in table, whole li is link

--- web/main.py
+++ web/main.py
@@ -31,12 +31,12 @@ def show(event):
     print(f'{event=}')
     if event == None: # just hack to call on load
         category_id = 'a'
-        #num = 0
+        category_text = 'Other traditions'
     else:
         dropdown = event.target
         category_id = dropdown.value 
-        #num = dropdown.selectedIndex
-        #category_name = dropdown.options[num].text
+        num = dropdown.selectedIndex
+        category_text = dropdown.options[num].text
 
     
     #dropdown = event.target
@@ -46,13 +46,18 @@ def show(event):
 
     # delete old questions
     document["zone2"].textContent = ""  
+    document["question_title"].textContent = f'Questions for category - "{category_text}":'  
 
+    counter = 0
     for qs, ql, link, cat in all_data:
         if category_id in cat:  
+            counter += 1
+
             # old way, just link separated by new line
             #document["zone2"] <= html.BR()
             #document["zone2"] <= html.A(qs, href=link, target="_blank")
 
+            """
             # Create a new row
             new_row = html.TR()
             # Create a cell (TD) element to wrap the anchor element
@@ -68,6 +73,14 @@ def show(event):
             # Add the cell to the new row
             new_row <= cell
             document["zone2"] <= new_row
+            """
+
+            new_row = html.A(qs, href=link, target="_blank", 
+                                Class="list-group-item list-group-item-action")
+            document["zone2"] <= new_row
+
+            if counter % 2 == True:
+                new_row.class_name += ' list-group-item-secondary' # must be space
 
             # ovo radi OK
             #document["zone2"] <= html.IFRAME(width="560", height="315",
