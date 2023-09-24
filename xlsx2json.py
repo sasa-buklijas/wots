16ac335df08b26211a3f2bd3f09fcf496a507049
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-09-24 09:34:56 +0200

    version 2

--- xlsx2json.py
+++ xlsx2json.py
@@ -1,9 +1,10 @@
 import openpyxl
 import json 
+import sys
 #import pprint
 
 def main():
-    file_path = 'WOTS_QnA.xlsx'
+    file_path = sys.argv[1]     # first arg is excel file 
     workbook = openpyxl.load_workbook(file_path)
 
     sheet_name = 'Sheet1'
@@ -11,8 +12,8 @@ def main():
 
     # make category
     category = dict()
-    G_column = 7 
-    H_column = G_column + 1 
+    G_column = 8                # QnA Topic letter (in old version vas 7)
+    H_column = G_column + 1     # QnA Topic name
     for category_id, category_name in sheet.iter_rows(min_row=2, min_col=G_column, max_col=H_column, values_only=True):
         #print(f'{row=}') 
         #print(f'{category_id=} {category_name=}')
@@ -28,17 +29,18 @@ def main():
     # make all
     all_data = list()
     # max_row=3 only 2 first, for testing
-    for qs, ql, links, _, category_ in sheet.iter_rows(min_row=2, max_col=5, values_only=True):#, max_row=3):
-        #print(f'{qs=}, {ql=}, {links=}, {category=}\n')
-
-        if category_ == None:
+    for qs, ql, links, _, _, _, category_id in sheet.iter_rows(min_row=2, max_col=7, values_only=True):#, max_row=3):
+        #print(f'{qs=}, {ql=}, {links=}, {category_id=}\n')
+        #break  # just for test
+        
+        if category_id == None:
             continue # this should be error
 
-        #if not all((qs, ql, links, category_)):
-        if not all((qs, links, category_)):
+        #if not all((qs, ql, links, category_id)):
+        if not all((qs, links, category_id)):
             break
 
-        all_data.append((qs, ql, links.split(',')[0], category_))
+        all_data.append((qs, ql, links.split(',')[0], category_id))
 
     # show when done
     all_json = json.dumps(all_data)
